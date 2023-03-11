b25692570b114f1aeb2e6b716d31d71c89976fee
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-03-11 20:55:45 +0200

    add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,4 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
@@ -9,7 +10,9 @@ function onPlay(e){
 localStorage.setItem(TIME_KEY, e.seconds);
 }
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
+if(TIME_KEY){
 player.setCurrentTime(localStorage.getItem(TIME_KEY));
+}
  
---
+++ src/js/03-feedback.js
@@ -0,0 +1,46 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const emailFormEl = document.querySelector('.feedback-form input');
+const messageFormEl = document.querySelector('.feedback-form textarea');
+
+const DATA_KEY = 'feedback-form-state';
+
+fillFormInput();
+
+formEl.addEventListener('submit', handleFormSubmit);
+
+formEl.addEventListener('input', throttle(handleFormInput, 500));
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  if (emailFormEl.textContent !== '' && messageFormEl.textContent !== '') {
+    if (DATA_KEY) {
+      console.log(JSON.parse(localStorage.getItem(DATA_KEY)));
+    }
+  }
+  e.currentTarget.reset();
+  localStorage.removeItem(DATA_KEY);
+}
+
+function handleFormInput(e) {
+  if (emailFormEl.value !== '' && messageFormEl.value !== '') {
+    localStorage.setItem(
+      DATA_KEY,
+      JSON.stringify({
+        email: `${emailFormEl.value}`,
+        message: `${messageFormEl.value}`,
+      })
+    );
+  }
+}
+
+function fillFormInput() {
+  const savedData = JSON.parse(localStorage.getItem(DATA_KEY));
+
+  if (savedData) {
+    emailFormEl.value === savedData.email;
+    messageFormEl.value === savedData.message;
+  }
+}
