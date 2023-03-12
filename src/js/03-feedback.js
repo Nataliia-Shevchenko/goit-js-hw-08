7a9a6aeaa67df778d288fb65820d211521fa1b9b
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-03-12 15:57:14 +0200

    add changes to task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,9 @@
 import throttle from 'lodash.throttle';
 
-
 const formEl = document.querySelector('.feedback-form');
 const emailFormEl = document.querySelector('.feedback-form input');
 const messageFormEl = document.querySelector('.feedback-form textarea');
+const btnEl = document.querySelector('.feedback-form button');
 
 const DATA_KEY = 'feedback-form-state';
 
@@ -15,20 +15,21 @@ formEl.addEventListener('input', throttle(handleFormInput, 500));
 
 function handleFormSubmit(e) {
   e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
 
-  if (e.target.elements.email.value === '' && e.target.elements.message.value === '') {
-    alert('Усі поля повинні бути заповнені!');
+  if (email.value === '' || message.value === '') {
+    return console.log('Please fill in all the fields!');
   }
 
   console.log(JSON.parse(localStorage.getItem(DATA_KEY)));
- 
 
   e.currentTarget.reset();
   localStorage.removeItem(DATA_KEY);
 }
 
 function handleFormInput(e) {
-
   if (emailFormEl.value !== '' || messageFormEl.value !== '') {
     localStorage.setItem(
       DATA_KEY,
@@ -44,7 +45,6 @@ function fillFormInput() {
   const savedData = JSON.parse(localStorage.getItem(DATA_KEY));
 
   if (savedData) {
-    
     emailFormEl.value = savedData.email;
     messageFormEl.value = savedData.message;
   }
