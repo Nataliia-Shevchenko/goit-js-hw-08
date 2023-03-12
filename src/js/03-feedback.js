2719f00dd519e85e3555bb59c8bb427d87f144be
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-03-12 15:08:14 +0200

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,6 @@
 import throttle from 'lodash.throttle';
 
+
 const formEl = document.querySelector('.feedback-form');
 const emailFormEl = document.querySelector('.feedback-form input');
 const messageFormEl = document.querySelector('.feedback-form textarea');
@@ -15,17 +16,20 @@ formEl.addEventListener('input', throttle(handleFormInput, 500));
 function handleFormSubmit(e) {
   e.preventDefault();
 
-  if (emailFormEl.textContent !== '' && messageFormEl.textContent !== '') {
-    if (DATA_KEY) {
-      console.log(JSON.parse(localStorage.getItem(DATA_KEY)));
-    }
+  if (e.target.elements.email.value === '' && e.target.elements.message.value === '') {
+    alert('Усі поля повинні бути заповнені!');
   }
+
+  console.log(JSON.parse(localStorage.getItem(DATA_KEY)));
+ 
+
   e.currentTarget.reset();
   localStorage.removeItem(DATA_KEY);
 }
 
 function handleFormInput(e) {
-  if (emailFormEl.value !== '' && messageFormEl.value !== '') {
+
+  if (emailFormEl.value !== '' || messageFormEl.value !== '') {
     localStorage.setItem(
       DATA_KEY,
       JSON.stringify({
@@ -40,7 +44,8 @@ function fillFormInput() {
   const savedData = JSON.parse(localStorage.getItem(DATA_KEY));
 
   if (savedData) {
-    emailFormEl.value === savedData.email;
-    messageFormEl.value === savedData.message;
+    
+    emailFormEl.value = savedData.email;
+    messageFormEl.value = savedData.message;
   }
 }
